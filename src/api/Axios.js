1d1ae02dea89674aa982f76f713a23f33f44c007
SridharArivu <sridhararivu368@gmail.com>
2023-12-08 10:24:04 +0530

    url path changed

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const Axios = axios.create({
     // baseURL: 'http://localhost:8080',
-    baseURL: 'https://orbital-nuance-403709.el.r.appspot.com/', 
+    baseURL: 'https://orbital-nuance-403709.el.r.appspot.com', 
     timeout: 20000,
     headers: {
       'Content-Type': 'application/json',
